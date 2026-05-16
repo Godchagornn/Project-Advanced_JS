7e4fc3d82f223fa24f3895b336569895ec2479c2
Godchagorn <pan.godchagorn2548@gmail.com>
2026-05-16 17:02:30 +0700

    Correct the error in data retrieval.

--- freshcart/vite.config.ts
+++ freshcart/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/products': 'http://localhost:3001',
+      '/cartItems': 'http://localhost:3001',
+      '/orders': 'http://localhost:3001',
+      '/reviews': 'http://localhost:3001',
+    },
+  },
 })
